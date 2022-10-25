14fc728f5008438292a44ff5d6917b2eac05d2ee
Pedro Barros <pedrobars7a@gmail.com>
2022-10-25 07:56:15 -0300

    Practicing components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
 import { ServerComponent } from "./server/server.component";
-import { ServersComponent } from './servers/servers.component';
+import { ServersComponent } from "./servers/servers.component";
+import { SuccesAlertComponent } from "./succes-alert/succes-alert.component";
+import { WarningAlertComponent } from "./warning-alert/warning-alert.component";
 
 @NgModule({
-  declarations: [AppComponent, ServerComponent, ServersComponent],
+  declarations: [
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    SuccesAlertComponent,
+    WarningAlertComponent,
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
