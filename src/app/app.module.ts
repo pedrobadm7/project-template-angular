cd0aa550e849ea0072a0138e1d267fe5b9577ba0
Pedro Barros <pedrobars7a@gmail.com>
2022-11-14 07:55:26 -0300

    Forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { ServerComponent } from "./server/server.component";
 import { ServersComponent } from "./servers/servers.component";
 import { SuccesAlertComponent } from "./succes-alert/succes-alert.component";
 import { WarningAlertComponent } from "./warning-alert/warning-alert.component";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -15,7 +16,7 @@ import { WarningAlertComponent } from "./warning-alert/warning-alert.component";
     SuccesAlertComponent,
     WarningAlertComponent,
   ],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
